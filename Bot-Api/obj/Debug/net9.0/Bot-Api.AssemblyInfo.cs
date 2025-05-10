601acd7fb4e00f607b986fbe7e4397b4afedf022
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-05-10 17:30:23 -0400

    Publict api test

--- Bot-Api/obj/Debug/net9.0/Bot-Api.AssemblyInfo.cs
+++ Bot-Api/obj/Debug/net9.0/Bot-Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bot-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+220aeff47f509dd61eaf6171a97849b775b1745f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6933a60cdf65f96d090e5cd7c29a49ae61fb516")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bot-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bot-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
