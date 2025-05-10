59057919135792356ec0b7d2ee33e7b689f21721
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-05-10 18:10:09 -0400

    Cambio de puerto de Bot-Apí

--- Bot-Api/obj/Release/net9.0/Bot-Api.AssemblyInfo.cs
+++ Bot-Api/obj/Release/net9.0/Bot-Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bot-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6933a60cdf65f96d090e5cd7c29a49ae61fb516")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39053089a9a2a46873247aa5795e3ca6c2bde9ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bot-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bot-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
