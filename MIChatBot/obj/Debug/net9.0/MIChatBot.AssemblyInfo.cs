d2b95a820294460d75ca1ae3c81e006cab3b07b7
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-05-27 14:57:26 -0400

    test (Git Action)

--- MIChatBot/obj/Debug/net9.0/MIChatBot.AssemblyInfo.cs
+++ MIChatBot/obj/Debug/net9.0/MIChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MIChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e199875996f94ea249042a6c5470b1720ef2a6cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9182dcc4a89cdc1a11a56b18518d4711a752244e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MIChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MIChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
