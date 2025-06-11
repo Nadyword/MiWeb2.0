5aabfa9d858c57eebe73f40368c1ce322e351ca2
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-06-11 10:28:04 -0400

    Git action

--- Mi Web/obj/Debug/net9.0/Mi Web.AssemblyInfo.cs
+++ Mi Web/obj/Debug/net9.0/Mi Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mi Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1e9feeecdb13b824a9a2376da510591aa251c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4905f923cc917b3b37b39a4fbfca297fb0bab52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mi Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mi Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- bin/bash/deploy.sh
+++ bin/bash/deploy.sh
@@ -1,26 +1,6 @@
-#!/bin/bash
-
-echo "📂 1. Entrando al directorio del proyecto"
-cd /var/www/MiWeb2.0/ || exit 1
-
-echo "🔄 2. Haciendo git pull"
-git pull || exit 1
-
-echo "⬆️ 3. Subiendo un nivel para limpiar despliegue anterior"
-cd .. || exit 1
-
-echo "🧹 4. Eliminando versión anterior publicada"
-rm -rf App/publish/
-
-echo "📁 5. Copiando nueva publicación a carpeta final"
-cp -r MiWeb2.0/MIChatBot/bin/Release/net9.0/publish/ /var/www/App
-
-echo "📂 6. Entrando al directorio de despliegue"
-cd App/publish/ || exit 1
-
-echo "🔓 7. Dando permisos de ejecución al binario"
-chmod +x MIChatBot
-
-echo "🚀 8. Reiniciando la apliacion"
-systemctl stop bot-api.service
-systemctl start bot-api.service
+cd /var/www/MiWeb2.0/
+git pull
+rm -r /var/www/blazor
+cd /var/www/MiWeb2.0/Mi Web/bin/Release/net9.0/publish
+cp -r wwwroot/ /var/www/blazor
+sudo systemctl restart nginx
