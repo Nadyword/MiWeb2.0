ec8f5a14077d87ac80694e2c22442e3f4227d393
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-06-11 10:39:20 -0400

    Update deploy.sh

--- bin/bash/deploy.sh
+++ bin/bash/deploy.sh
@@ -1,6 +1,6 @@
 cd /var/www/MiWeb2.0/
 git pull
 rm -r /var/www/blazor
-cd /var/www/MiWeb2.0/Mi Web/bin/Release/net9.0/publish
+cd /var/www/MiWeb2.0/Mi\ Web/bin/Release/net9.0/publish/
 cp -r wwwroot/ /var/www/blazor
 sudo systemctl restart nginx
