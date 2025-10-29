51dddf3770b9d55d5b631736a9616e4c3510555b
Nadyword <83626464+Nadyword@users.noreply.github.com>
2025-10-29 16:54:43 -0300

    Borrado de cambio otro proyectos

--- Api inmobiliaria/Models/RequestJwt.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Api_inmobiliaria.Models
-{
-    public class RequestJwt<T>
-    {
-        public required string Token { get; set; }
-        public required T Data { get; set; }
-    }
-}
--- bin/bash/deploy.sh
+++
@@ -1,8 +0,0 @@
-cd /var/www/MiWeb2.0/
-git pull
-rm -r /var/www/blazor
-cd /var/www/MiWeb2.0/Mi\ Web/bin/Release/net9.0/publish/
-cp -r wwwroot/ /var/www/blazor
-sudo systemctl restart nginx
-sudo systemctl reload nginx
-sudo rm -rf /var/cache/nginx/*
